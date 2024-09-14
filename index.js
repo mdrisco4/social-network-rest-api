82f3db3238bf2900a3b517420306e7db75e12f17
michaeldriscoll <michaelndriscoll81@gmail.com>
2024-09-13 19:52:01 -0700

    running into issues connecting with the DB.  investigating and researching the errors I am receiving.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// import MongoStore from 'connect-mongo';
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -7,7 +9,52 @@ const morgan = require("morgan");
 
 dotenv.config();
 
+// mongoose.connect(
+//     process.env.MONGO_URL,{ 
+//         useNewUrlParser: true, 
+//         useUnifiedTopology: true }, 
+//     () => {
+//     console.log("Connected to MongoDB");
+// });
 
-app.listen(8800,()=>{
-    console.log("backend server is running!")
-})
+// mongoose
+//   .connect(process.env.MONGO_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then((res) => {
+//     console.log("Database connected");
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://User_1:<db_password>@cluster0.2bwljqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(8800, () => {
+  console.log("backend server is running!");
+});
