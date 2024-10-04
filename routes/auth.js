058577a617ade41ffda36319e7f6f1dd5bb9c956
michaeldriscoll <michaelndriscoll81@gmail.com>
2024-10-04 16:08:18 -0700

    refactoring some code

--- routes/auth.js
+++ routes/auth.js
@@ -1,44 +1,6 @@
-// const router = require("express").Router();
-// const User = require("../models/User")
-
-
-// router.get("/",(req,res)=>{
-//     res.send("welcome to auth routes")
-// })
-
-// REGISTER
-
-// router.get("/register", async (req, res) => {
-//     const user = await new User({
-//         username:"John",
-//         email:"john@gmail.com",
-//         password:"123456"
-//     })
-
-//     await user.save();
-//     res.send("OK")
-// });
-
-
-// router.post("/register", async (req, res) => {
-//     const user = new User({
-//         username:"John",
-//         email:"john@gmail.com",
-//         password:"123456"
-//     });
-
-//     try {
-//         await user.save();
-//         res.status(201).send("User registered successfully");
-//     } catch (error) {
-//         console.error("Error saving user:", error);
-//         res.status(500).send("Error saving user");
-//     }
-// });
-
-
 const router = require("express").Router();
 const User = require("../models/User")
+// router.use(express.json())
 
 
 router.post("/register", async (req, res) => {
